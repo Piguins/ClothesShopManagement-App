25c96df5174b66478f1b0436c4b689aa898b9a75
Kiethongngu <21522263@gm.uit.edu.vn>
2022-11-23 11:28:29 +0700

    Đang làm dở chức năng logout

--- Clothes Shop Management Demo/Demo/View/LoginViewPage.xaml.cs
+++ Clothes Shop Management Demo/Demo/View/LoginViewPage.xaml.cs
@@ -35,6 +35,7 @@ namespace Demo.View
         {
             MainView mainView = new MainView();
             mainView.Show();
+            
         }
     }
 }
--- Clothes Shop Management Demo/Demo/ViewModel/MainViewModel.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/MainViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Demo.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Linq;
 
 namespace Demo.ViewModel
 {
     internal class MainViewModel
     {
+        public ICommand LogOutCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LogOutCommand = new RelayCommand<MainWindow>((p) => { return true; }, (p) => LogOut(p));
+        }
+        void LogOut(MainWindow p)
+        {
+            LoginView login = new LoginView();
+            login.Show();
+        }
     }
 }
