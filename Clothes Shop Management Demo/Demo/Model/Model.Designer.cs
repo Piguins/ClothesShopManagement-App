2c7f0a272c144435ad461f8c3f46c4132ce32da3
Kiethongngu <21522263@gm.uit.edu.vn>
2022-12-05 17:11:12 +0700

    Update button Voucher, phát triển sau này

--- Clothes Shop Management Demo/Demo/Model/Model.Designer.cs
+++ Clothes Shop Management Demo/Demo/Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\theki\OneDrive\Desktop\Clothes-Shop-Management-Project\Clothes Shop Management Demo\Demo\Model\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Clothes-Shop-Management-Project\Clothes Shop Management Demo\Demo\Model\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
