f85b03010a773404db7df78012e56aff5365d956
Kiethongngu <21522263@gm.uit.edu.vn>
2022-12-05 14:45:07 +0700

    Done xử lý chức năng với database Details Order

--- Clothes Shop Management Demo/Demo/ViewModel/OrderViewModel.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/OrderViewModel.cs
@@ -26,7 +26,7 @@ namespace Demo.ViewModel
             listTK = new ObservableCollection<string>() { "Họ tên", "Số HD", "Ngày" };
             listHD = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
             SearchCommand = new RelayCommand<OrderView>((p) => true, (p) => _SearchCommand(p));
-            //Detail = new RelayCommand<OrderView>((p) => p.ListViewHD.SelectedItem != null ? true : false, (p) => _Detail(p));
+            Detail = new RelayCommand<OrderView>((p) => p.ListViewHD.SelectedItem != null ? true : false, (p) => _Detail(p));
             LoadCsCommand = new RelayCommand<OrderView>((p) => true, (p) => _LoadCsCommand(p));
         }
         void _LoadCsCommand(OrderView parameter)
@@ -115,31 +115,32 @@ namespace Demo.ViewModel
             else
                 paramater.ListViewHD.ItemsSource = listHD;
         }
-        //void _Detail(OrderView parameter)
-        //{
-        //    LoginViewModel.MainFrame.Content = new DetailsOrder();
-        //    DetailsOrder = p;
-        //    HOADON temp = (HOADON)parameter.ListViewHD.SelectedItem;
-        //    p.MaND.Text = temp.NGUOIDUNG.MAND;
-        //    p.TenND.Text = temp.NGUOIDUNG.TENND;
-        //    p.Ngay.Text = temp.NGHD.ToString("dd/MM/yyyy hh:mm tt");
-        //    p.SoHD.Text = temp.SOHD.ToString();
-        //    p.MaKH.Text = temp.MAKH.ToString();
-        //    p.TenKH.Text = temp.KHACHHANG.HOTEN;
-        //    p.KM.Text = temp.KHUYENMAI.ToString() + "%";
-        //    List<HienThi> list = new List<HienThi>();
-        //    foreach (CTHD a in temp.CTHDs)
-        //    {
-        //        list.Add(new HienThi(a.MASP, a.SANPHAM.TENSP, a.SANPHAM.SIZE, a.SL, a.SANPHAM.GIA, a.SL * a.SANPHAM.GIA));
-        //    }
-        //    p.ListViewSP.ItemsSource = list;
-        //    p.GG.Text = "- " + String.Format("{0:0,0}", (temp.TRIGIA * 100 / (100 - temp.KHUYENMAI)) * temp.KHUYENMAI / 100) + " VND";
-        //    p.TT.Text = String.Format("{0:0,0}", temp.TRIGIA) + " VND";
-        //    p.TT1.Text = String.Format("{0:0,0}", temp.TRIGIA) + " VND";
-        //    parameter.ListViewHD.SelectedItem = null;
-        //    listHD = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
-        //    parameter.ListViewHD.ItemsSource = listHD;
-        //    _SearchCommand(parameter);
-        //}
+        void _Detail(OrderView parameter)
+        {
+            DetailsOrder p = new DetailsOrder();
+
+            HOADON temp = (HOADON)parameter.ListViewHD.SelectedItem;
+            p.MaND.Text = temp.NGUOIDUNG.MAND;
+            p.TenND.Text = temp.NGUOIDUNG.TENND;
+            p.Ngay.Text = temp.NGHD.ToString("dd/MM/yyyy hh:mm tt");
+            p.SoHD.Text = temp.SOHD.ToString();
+            p.MaKH.Text = temp.MAKH.ToString();
+            p.TenKH.Text = temp.KHACHHANG.HOTEN;
+            p.KM.Text = temp.KHUYENMAI.ToString() + "%";
+            List<HienThi> list = new List<HienThi>();
+            foreach (CTHD a in temp.CTHDs)
+            {
+                list.Add(new HienThi(a.MASP, a.SANPHAM.TENSP, a.SANPHAM.SIZE, a.SL, a.SANPHAM.GIA, a.SL * a.SANPHAM.GIA));
+            }
+            p.ListViewSP.ItemsSource = list;
+            p.GG.Text = "- " + String.Format("{0:0,0}", (temp.TRIGIA * 100 / (100 - temp.KHUYENMAI)) * temp.KHUYENMAI / 100) + " VND";
+            p.TT.Text = String.Format("{0:0,0}", temp.TRIGIA) + " VND";
+            p.TT1.Text = String.Format("{0:0,0}", temp.TRIGIA) + " VND";
+            parameter.ListViewHD.SelectedItem = null;
+            listHD = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
+            parameter.ListViewHD.ItemsSource = listHD;
+            _SearchCommand(parameter);
+            MainViewModel.MainFrame.Content = p;
+        }
     }
 }
