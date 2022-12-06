19e4df9b05f5f62c390308d0fc174dbf754785a1
Minguml <vhnm3004@outlook.com>
2022-12-06 19:21:55 +0700

    Sửa bug trong Register + Xong Quên MK

--- Clothes Shop Management Demo/Demo/ViewModel/ForgetPassViewModel.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/ForgetPassViewModel.cs
@@ -1,10 +1,14 @@
-﻿using Demo.View;
+﻿using Demo.Model;
+using Demo.View;
 using Demo.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Demo.ViewModel
@@ -12,6 +16,7 @@ namespace Demo.ViewModel
     public class ForgetPassViewModel : BaseViewModel
     {
         public ICommand CancelCM { get; set; }
+        public ICommand SendPass { get; set; }
 
         public ForgetPassViewModel()
         {
@@ -19,6 +24,34 @@ namespace Demo.ViewModel
             {
                 LoginViewModel.MainFrame.Content = new LoginViewPage();
             });
+            SendPass = new RelayCommand<ForgetPassView>((p) => true, (p) => _SendPass(p));
+        }
+        void _SendPass(ForgetPassView parameter)
+        {
+            int dem = DataProvider.Ins.DB.NGUOIDUNGs.Where(p => p.MAIL == parameter.MailAddress.Text).Count();
+            if (dem == 0)
+            {
+                MessageBox.Show("Email này chưa được đăng lý !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Random rand = new Random();
+            string newpass = rand.Next(100000, 999999).ToString();
+            foreach (NGUOIDUNG temp in DataProvider.Ins.DB.NGUOIDUNGs)
+            {
+                if (temp.MAIL == parameter.MailAddress.Text)
+                {
+                    temp.PASS = LoginViewModel.MD5Hash(LoginViewModel.Base64Encode(newpass));
+                    break;
+                }
+            }
+            DataProvider.Ins.DB.SaveChanges();
+            string nd = "Vui lòng nhập mật khẩu " + newpass + " để đăng nhập. Trân trọng !";
+            MailMessage message = new MailMessage("doanit008@gmail.com", parameter.MailAddress.Text, "Lấy lại mật khẩu", nd);
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential("doanit008@gmail.com", "laptrinhtructiu");
+            smtpClient.Send(message);
+            MessageBox.Show("Đã gửi mật khẩu vào Email đăng ký !", "Thông báo");
         }
     }
 }
