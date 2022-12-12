84bab195e879fcd7b7f0ea0160ca61cfc5cc9b1e
Kiethongngu <21522263@gm.uit.edu.vn>
2022-12-12 16:19:33 +0700

    Setting đang bị bug, chưa sửa được

--- Clothes Shop Management Demo/Demo/ViewModel/AddNDVM.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/AddNDVM.cs
@@ -1,4 +1,5 @@
-﻿using Demo.View;
+﻿using Demo.Model;
+using Demo.View;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -25,27 +26,136 @@ namespace Demo.ViewModel
         public static Frame MainFrame { get; set; }
         public AddNDVM()
         {
+            linkaddimage = Const._localLink + "/Resource/Image/addava.png";
+            AddNDCommand = new RelayCommand<AddNDView>((p) => true, (p) => _AddND(p));
+            AddImage = new RelayCommand<ImageBrush>((p) => true, (p) => _AddImage(p));
+
             CancelCM = new RelayCommand<object>((p) => { return p == null ? false : true; }, (p) =>
             {
                 MainViewModel.MainFrame.Content = new QLNVView();
             });
-            //linkaddimage = Const._localLink + "/Resource/Image/addava.png";
-            //AddImage = new RelayCommand<ImageBrush>((p) => true, (p) => _AddImage(p));
-        }
-        /*void _AddImage(ImageBrush img)
-        {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files(*.jpg; *.png)|*.jpg; *.png";
 
-            if (open.ShowDialog() == true)
+            void _AddImage(ImageBrush img)
             {
-                if (open.FileName != "")
-                    linkaddimage = open.FileName;
-            };
-            Uri fileUri = new Uri(linkaddimage);
-            img.ImageSource = new BitmapImage(fileUri);
-        }*/ //Cái này là để thêm hình & lưu vào database
-            //nhưng mà mình chưa implement Model nên t (Minh)
-            //sẽ code tạm là show hình trên cái khung hình đại diện trong cái phần AddNDView.xaml.c
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.jpg; *.png)|*.jpg; *.png";
+
+                if (open.ShowDialog() == true)
+                {
+                    if (open.FileName != "")
+                        linkaddimage = open.FileName;
+                };
+                Uri fileUri = new Uri(linkaddimage);
+                img.ImageSource = new BitmapImage(fileUri);
+            }
+
+            bool check(string m)
+            {
+                foreach (NGUOIDUNG temp in DataProvider.Ins.DB.NGUOIDUNGs)
+                {
+                    if (temp.MAND == m)
+                        return true;
+                }
+                return false;
+            }
+
+            string rdma()
+            {
+                string ma;
+                do
+                {
+                    Random rand = new Random();
+                    ma = "NV" + rand.Next(0, 10000).ToString();
+                } while (check(ma));
+                return ma;
+            }
+            void _AddND(AddNDView addNDView)
+            {
+                MessageBoxResult h = System.Windows.MessageBox.Show("Bạn muốn thêm người dùng ?", "THÔNG BÁO", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+                if (h == MessageBoxResult.Yes)
+                {
+                    if (String.IsNullOrEmpty(addNDView.MaND.Text) || String.IsNullOrEmpty(addNDView.TenND.Text) || String.IsNullOrEmpty(addNDView.SDT.Text) || String.IsNullOrEmpty(addNDView.GT.Text) || String.IsNullOrEmpty(addNDView.QTV.Text) || addNDView.NS.SelectedDate == null)
+                    {
+                        MessageBox.Show("Bạn chưa nhập đầy đủ thông tin !", "THÔNG BÁO");
+                        return;
+                    }
+                    NGUOIDUNG temp = new NGUOIDUNG();
+                    foreach (NGUOIDUNG a in DataProvider.Ins.DB.NGUOIDUNGs.Where(p => p.TTND == true))
+                    {
+                        if (addNDView.MaND.Text == a.MAND)
+                        {
+                            MessageBox.Show("Mã ND đã tồn tại !", "THÔNG BÁO");
+                            return;
+                        }
+                    }
+                    foreach (NGUOIDUNG temp5 in DataProvider.Ins.DB.NGUOIDUNGs)
+                    {
+                        if (temp5.MAIL == addNDView.Mail.Text)
+                        {
+                            MessageBox.Show("Email này đã được sử dụng !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                    }
+                    string match = @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+                    Regex reg = new Regex(match);
+                    if (!reg.IsMatch(addNDView.Mail.Text))
+                    {
+                        MessageBox.Show("Email không hợp lệ !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    string match1 = @"^((09(\d){8})|(086(\d){7})|(088(\d){7})|(089(\d){7})|(01(\d){9}))$";
+                    Regex reg1 = new Regex(match1);
+                    if (!reg1.IsMatch(addNDView.SDT.Text))
+                    {
+                        MessageBox.Show("Số điện thoại không hợp lệ !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    temp.MAND = addNDView.MaND.Text;
+                    temp.TENND = addNDView.TenND.Text;
+                    temp.SDT = addNDView.SDT.Text;
+                    temp.DIACHI = addNDView.DC.Text;
+                    temp.GIOITINH = addNDView.GT.Text;
+                    temp.MAIL = addNDView.Mail.Text;
+                    temp.NGSINH = (DateTime)addNDView.NS.SelectedDate;
+                    if (addNDView.QTV.Text == "Quản lý")
+                        temp.QTV = true;
+                    else
+                        temp.QTV = false;
+                    temp.TTND = true;
+                    temp.USERNAME = addNDView.MaND.Text;
+                    temp.PASS = LoginViewModel.MD5Hash(LoginViewModel.Base64Encode(addNDView.MaND.Text));
+                    if (linkaddimage == "/Resource/Image/addava.png")
+                        temp.AVA = "/Resource/Image/addava.png";
+                    else
+                        temp.AVA = "/Resource/Ava/" + addNDView.MaND.Text + ((linkaddimage.Contains(".jpg")) ? ".jpg" : ".png").ToString();
+                    DataProvider.Ins.DB.NGUOIDUNGs.Add(temp);
+                    try
+                    {
+                        File.Copy(linkaddimage, Const._localLink + @"Resource\Ava\" + temp.MAND + ((linkaddimage.Contains(".jpg")) ? ".jpg" : ".png").ToString(), true);
+                    }
+                    catch { }
+                    DataProvider.Ins.DB.SaveChanges();
+                    MessageBox.Show("Thêm người dùng thành công !", "THÔNG BÁO");
+                    addNDView.MaND.Text = rdma();
+                    addNDView.TenND.Clear();
+                    addNDView.GT.SelectedItem = null;
+                    addNDView.GT.Items.Refresh();
+                    addNDView.QTV.SelectedItem = null;
+                    addNDView.QTV.Items.Refresh();
+                    addNDView.NS.SelectedDate = null;
+                    addNDView.SDT.Clear();
+                    addNDView.DC.Clear();
+                    addNDView.Mail.Clear();
+                    linkaddimage = Const._localLink + "/Resource/Image/addava.png";
+                    Uri fileUri = new Uri(linkaddimage);
+                    addNDView.HinhAnh1.ImageSource = new BitmapImage(fileUri);
+                    QLNVView qLNVView = new QLNVView();
+                    MainViewModel.MainFrame.Content = qLNVView;
+                }
+            }
+        }
     }
 }
+
+
+
--- Clothes Shop Management Demo/Demo/ViewModel/SettingViewModel.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/SettingViewModel.cs
@@ -1,12 +1,132 @@
-﻿using System;
+﻿using Demo.View;
+using Demo.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+using System.Text.RegularExpressions;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
 
 namespace Demo.ViewModel
 {
-    internal class SettingViewModel
+    public class SettingViewModel : BaseViewModel
     {
+        private string _Ava;
+        public string Ava { get => _Ava; set { _Ava = value; OnPropertyChanged(); } }
+        private string _Name;
+        public string Name { get => _Name; set { _Name = value; OnPropertyChanged(); } }
+        private string _DoB;
+        public string DoB { get => _DoB; set { _DoB = value; OnPropertyChanged(); } }
+        private string _DiaChi;
+        public string DiaChi { get => _DiaChi; set { _DiaChi = value; OnPropertyChanged(); } }
+        private string _Mail;
+        public string Mail { get => _Mail; set { _Mail = value; OnPropertyChanged(); } }
+        private int _GioiTinh;
+        public int GioiTinh { get => _GioiTinh; set { _GioiTinh = value; OnPropertyChanged(); } }
+        private string _SDT;
+        public string SDT { get => _SDT; set { _SDT = value; OnPropertyChanged(); } }
+        private string _TenTK;
+        public string TenTK { get => _TenTK; set { _TenTK = value; OnPropertyChanged(); } }
+        private NGUOIDUNG _User;
+        public NGUOIDUNG User { get => _User; set { _User = value; OnPropertyChanged(); } }
+        public ICommand Loadwd { get; set; }
+        public ICommand UpdateInfo { get; set; }
+        public ICommand AddImage { get; set; }
+        public ICommand ChangePass { get; set; }
+        public SettingViewModel()
+        {
+            Loadwd = new RelayCommand<SettingView>((p) => true, (p) => _Loadwd(p));
+            //AddImage = new RelayCommand<ImageBrush>((p) => true, (p) => _AddImage(p));
+            UpdateInfo = new RelayCommand<SettingView>((p) => true, (p) => _UdpateInfo(p));
+            ChangePass = new RelayCommand<SettingView>((p) => true, (p) => _ChangePass());
+        }
+        void _ChangePass()
+        {
+            ChangePassword change = new ChangePassword();
+            MainViewModel.MainFrame.Content = change;
+        }
+        //void _AddImage(ImageBrush p)
+        //{
+        //    OpenFileDialog open = new OpenFileDialog();
+        //    open.Filter = "Image Files(*.jpg; *.png)|*.jpg; *.png";
+        //    if (open.ShowDialog() == true)
+        //    {
+        //        Ava = open.FileName;
+        //    }
+        //    p.ImageSource = new BitmapImage(new Uri(Ava));
+        //}
+        void _Loadwd(SettingView p)
+        {
+            if (LoginViewModel.IsLogin)
+            {
+                string a = Const.TenDangNhap;
+                User = DataProvider.Ins.DB.NGUOIDUNGs.Where(x => x.USERNAME == a).FirstOrDefault();
+                Ava = User.AVA;
+                Name = User.TENND;
+                DoB = User.NGSINH.ToString();
+                DiaChi = User.DIACHI;
+                GioiTinh = (User.GIOITINH == "Nam") ? 0 : 1;
+                SDT = User.SDT;
+                TenTK = User.USERNAME;
+                Mail = User.MAIL;
+            }
+        }
+        void _UdpateInfo(SettingView p)
+        {
+            foreach (NGUOIDUNG temp2 in DataProvider.Ins.DB.NGUOIDUNGs)
+            {
+                if (temp2.MAIL == p.Mail.Text && p.Mail.Text != Const.ND.MAIL)
+                {
+                    MessageBox.Show("Email này đã được sử dụng !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            string match = @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+            Regex reg = new Regex(match);
+            if (!reg.IsMatch(p.Mail.Text))
+            {
+                MessageBox.Show("Email không hợp lệ !", "THÔNG BÁO", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var temp = DataProvider.Ins.DB.NGUOIDUNGs.Where(pa => pa.USERNAME == TenTK).FirstOrDefault();
+            temp.TENND = p.TenND.Text;
+            temp.SDT = p.SDT.Text;
+            temp.DIACHI = p.DC.Text;
+            temp.GIOITINH = p.GT.Text;
+            temp.NGSINH = (DateTime)p.NS.SelectedDate;
+            temp.MAIL = p.Mail.Text;
+            string rd = StringGenerator();
+            if (User.AVA != Ava)
+                temp.AVA = "/Resource/Ava/" + rd + (Ava.Contains(".jpg") ? ".jpg" : ".png").ToString();
+            DataProvider.Ins.DB.SaveChanges();
+            //try
+            //{
+            //    if (User.AVA != Ava)
+            //        File.Copy(Ava, Const._localLink + @"Resource/Ava/" + rd + (Ava.Contains(".jpg") ? ".jpg" : ".png").ToString(), true);
+            //}
+            //catch { }
+            MessageBox.Show("Cập nhật thành công!", "Thông báo");
+        }
+        static string StringGenerator()
+        {
+            Random rd = new Random();
+            int length = rd.Next(5, 20);
+            StringBuilder str_build = new StringBuilder();
+            Random random = new Random();
+            char letter;
+            for (int i = 0; i < length; i++)
+            {
+                double flt = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                str_build.Append(letter);
+            }
+            return str_build.ToString();
+        }
     }
 }
