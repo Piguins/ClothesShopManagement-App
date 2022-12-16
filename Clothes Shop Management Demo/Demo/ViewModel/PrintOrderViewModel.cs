9ee64070a6d0fcbd4b2eed429689ed3fb1d72ed2
huuthien <huuthienngo0910@gmail.com>
2022-12-16 16:00:35 +0700

    sua UI thong tin hoa don

--- Clothes Shop Management Demo/Demo/ViewModel/PrintOrderViewModel.cs
+++ Clothes Shop Management Demo/Demo/ViewModel/PrintOrderViewModel.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Demo.Model;
+using Demo.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
 
 namespace Demo.ViewModel
 {
     internal class PrintOrderViewModel: BaseViewModel
     {
-
+        private ObservableCollection<KHACHHANG> _listKH;
+        public ObservableCollection<KHACHHANG> listKH { get => _listKH; set { _listKH = value; OnPropertyChanged(); } }
+        private ObservableCollection<string> _listTK;
+        public ObservableCollection<string> listTK { get => _listTK; set { _listTK = value; OnPropertyChanged(); } }
+        public PrintOrderViewModel()
+        {
+            listKH = new ObservableCollection<KHACHHANG>(DataProvider.Ins.DB.KHACHHANGs);
+            listTK = new ObservableCollection<string>() { "Họ tên", "Địa Chỉ", "SĐT" };
+        }
     }
 }
